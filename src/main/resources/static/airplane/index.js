29062d6f7f2f31a9d2f5f1ac1dfd9da5f18c7a76
SHONING <sietske.honing@capgemini.com>
2018-05-15 10:47:33 +0200

    Airplane page first setup, some todo's left

---
+++ src/main/resources/static/airplane/index.js
@@ -0,0 +1,52 @@
+function postNewAirplane(){
+    var plane = {
+        airplaneId :$("#name").val(),
+        fuelInTons :$("#fuel").val(),
+    }
+    $.post("http://localhost:8080/api/airplane/new", plane, function(result) {
+            getData();
+    });
+}
+
+function getData() {
+      // Get the data from endpoint.
+      $.ajax({
+        url:"http://localhost:8080/api/airplane/all",
+        type:"get",
+        success: function(data) {
+            // On successful get, reload the datatable with new data.
+            $('#table_planes').DataTable().clear();
+            $('#table_planes').DataTable().rows.add(data);
+            $('#table_planes').DataTable().columns.adjust().draw();
+        }
+    });
+}
+
+$(document).ready(function () {
+    // Modal Setup
+//    setSelectors(); MAKE A SETSELECTORS FOR THE AIRPORTS
+
+      // Modal submit.
+        $("#newAirplaneForm").on('submit', function(e) {
+                postNewAirplane();
+
+                // Reset modal to hide and no values.
+                $('#newAirplaneModal').modal('hide');
+
+                $("#name").val("");
+                $("#fuel").val("");
+                //$("#airport-selector").val("");
+        });
+
+    // Load DataTable with data format.
+    $('#table_planes').DataTable({
+        columns: [
+            { "data": "airplaneId" },
+            { "data": "fuelInTons" },
+//            { "data": "airport.name" },
+        ]
+    });
+
+    // Load first data.
+    getData();
+});
